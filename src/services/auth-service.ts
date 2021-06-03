12ed8b451bd0dfe250b98feaae1aeea5f0eaae31
MariSpirandelli <mariannaspirandelli@gmail.com>
2021-06-03 20:17:31 -0300

    auth service

---
+++ src/services/auth-service.ts
@@ -0,0 +1,57 @@
+import jwtDecode from "jwt-decode"
+import Router from 'next/router'
+import { redirectToLogin } from './redirect-service'
+
+export const TOKEN_STORAGE_KEY = 'myApp.authToken'
+
+export type DecodedToken = {
+  readonly email: string
+  readonly exp: number
+}
+
+export class AuthService {
+  decodedToken?: DecodedToken
+  token?: string
+
+  constructor() {
+    try {
+      this.token = localStorage.getItem(TOKEN_STORAGE_KEY) || undefined;
+      if (this.token) {
+        this.decodedToken = jwtDecode(this.token)
+      }
+    } catch (e) {}
+  }
+
+  get authorizationString() {
+    return `Bearer ${this.token}`
+  }
+
+  get expiresAt(): Date {
+    if (!this.decodedToken) {
+      return new Date(0)
+    }
+    return new Date(this.decodedToken.exp * 1000)
+  }
+
+  get isExpired(): boolean {
+    return new Date() > this.expiresAt
+  }
+
+  get isValid(): boolean {
+    return !this.isExpired
+  }
+
+  async storeToken(token: string) {
+    localStorage.setItem(TOKEN_STORAGE_KEY, token)
+    this.token = token
+    this.decodedToken = jwtDecode(token)
+    await Router.push('/products?name=Logged')
+  }
+
+  async logout() {
+    localStorage.removeItem(TOKEN_STORAGE_KEY)
+    await redirectToLogin()
+  }
+}
+
+export default new AuthService()
