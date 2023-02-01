52968932e7021d87fecac1272cb7922401d06e73
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-01 15:36:44 -0300

    formatting and small improvement

--- src/components/layout/menu-top/index.tsx
+++ src/components/layout/menu-top/index.tsx
@@ -1,29 +1,27 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { useSE } from '../../../SEProvider'
-import Logo from '../logo/index'
-import Logout from '../logout'
+import React, { Fragment, useEffect, useState } from 'react';
+import { useSE } from '../../../SEProvider';
+import Logo from '../logo/index';
+import Logout from '../logout';
 
 export default function Header() {
-  const se = useSE()
+  const se = useSE();
 
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    se.state.isAuthenticated
-  )
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   useEffect(() => {
-    setIsAuthenticated(se.state.isAuthenticated)
-  }, [se.state.isAuthenticated])
+    setIsAuthenticated(se.state.isAuthenticated);
+  }, [se.state.isAuthenticated]);
 
   return (
     <Fragment>
       <nav
         className={`navbar sticky-top navbar-expand-lg navbar-light ${
-          isAuthenticated && 'authenticated' || 'public-view'
+          (isAuthenticated && 'authenticated') || 'public-view'
         }`}
       >
         <Logo />
         {isAuthenticated && <Logout />}
       </nav>
     </Fragment>
-  )
+  );
 }
