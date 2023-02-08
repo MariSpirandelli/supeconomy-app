79df052f465c03c673c1ebbadcd2f3f8f05c3f86
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-08 15:41:40 -0300

    feat: tab panel component

---
+++ src/components/layout/tab-panel/index.tsx
@@ -0,0 +1,28 @@
+import Box from '@mui/material/Box';
+
+interface Props {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel: React.FC<Props> = ({
+  children,
+  value,
+  index,
+  ...other
+}: Props) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+};
+
+export default TabPanel;
